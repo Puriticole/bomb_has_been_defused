69128ba5d4a3df3c74c4e90e0118f64945ea2d4d
cedric_ard <cedric.airaudo@gmail.com>
2023-11-23 11:05:02 +0100

    Mis à jour de l'index

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -432,8 +432,7 @@ static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 /* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */0.03
-
+/* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
